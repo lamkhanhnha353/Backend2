813051766a8976cf594e58c364c5526de8d109e0
lamkhanhnha353 <lamkhanhnha353@gmail.com>
2025-10-01 22:53:05 +0700

    init hello world

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Định nghĩa route GET '/'
+app.get('/', (req, res) => {
+  res.send('Hello World với Express!')
+})
+
+// Lắng nghe port 3000
+app.listen(port, () => {
+  console.log(`App chạy tại http://localhost:${port}`)
+})
