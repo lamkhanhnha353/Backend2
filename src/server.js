6e7400b25eb153cd5bb1301350b70456045b4c2a
lamkhanhnha353 <lamkhanhnha353@gmail.com>
2025-10-02 00:57:33 +0700

    video 9 f8

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const port = 3000;
+const { engine } = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+//cau hình handlebars
+app.engine('hbs', engine({
+    extname: '.hbs' // đổi đuôi file mặc định .handlebars -> .hbs
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+// Định nghĩa route GET '/'
+app.get('/', (req, res) => {
+  res.render('home.hbs');
+})
+
+app.get('/news', (req, res)=>{
+  res.render('news.hbs');
+})
+
+// Lắng nghe port 3000
+app.listen(port, () => {
+  console.log(`App chạy tại http://localhost:${port}`)
+})
